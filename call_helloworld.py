f5c9e6dc0b0a0b317aa843f766cd1f75b380cc8a
shimi3435 <t-shimizu@stu.kobe-u.ac.jp>
2024-10-30 17:06:44 +0900

    helloworldを呼び出すcall_helloworld.pyの実装

---
+++ call_helloworld.py
@@ -0,0 +1,11 @@
+import helloworld
+
+def print_my_status():
+    print("I am a Python script.")
+
+def main():
+    helloworld.print_helloworld()
+    print_my_status()
+
+if __name__ == "__main__":
+    main()
